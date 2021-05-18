6e3e4e1adf5cbb7eb033709e7aa4bb51fcbc4e8b
qitas <qitas@qitas.cn>
2021-05-18 09:06:27 +0800

    fix

---
+++ UART/src/spi-uart-test.ino
@@ -0,0 +1,53 @@
+/*******************************************************************************
+****版本：V1.0.0
+****平台：P117
+****日期：2021-02-03
+****作者：Qitas
+****版权：OS-Q
+*******************************************************************************/
+#include <Arduino.h>
+#include "SPI_UART.h"
+
+char buffer[3];
+
+SPISettings spisettings = SPISettings(16000000, MSBFIRST, SPI_MODE0);
+
+void setup()
+{
+#ifdef __LGT8F__
+  // we use the alternate pins D5 and D6, because we also want to use the Serial for debugging
+    SPI_UART.useAlternatePins = true;
+#endif
+    buffer[0] = 0x55;
+    buffer[1] = 0;
+}
+
+
+// THIS loop only makes sense on LGT8F with remapped pins ;)
+void loop()
+{
+    buffer[2]++;
+    // we do some SPI on D5/D6:
+    SPI_UART.begin();
+    SPI_UART.beginTransaction(spisettings);
+    // now, you would do chip select before sending data
+    SPI_UART.transfer(buffer, 3);
+    SPI_UART.transfer(buffer, 3);
+    // and de-chip-select after sending/receiving data
+    SPI_UART.endTransaction();
+    SPI_UART.end();
+
+    // and then do some Serial debugging output at D0/D1:
+    if ((buffer[2] % 40) == 0)
+    {
+        Serial.begin(115200);
+        Serial.println(buffer[2], DEC);
+        Serial.end();
+    }
+    else
+    {
+        delay(5);
+    }
+}
+
+/*---------------------------(C) COPYRIGHT 2021 OS-Q -------------------------*/
---
+++ voltage/lgt8f328p_voltage/lgt8f328p_voltage.ino
@@ -0,0 +1,44 @@
+/*
+ReadAnalogVoltage
+ */
+// the setup routine runs once when you press reset:
+int light; //读取数值
+int max = 4000; //转换数值上限
+int min = 3200; //转换数值下限
+void setup() {
+	// initialize serial communication at 9600 bits per second:
+	Serial.begin(9600);
+	// analogReference(INTERNAL1V1);//use internal 1.1v as Avref
+	//analogReference(EXTERNAL); //使用外部基准
+
+//analogReference(type)
+//描述
+//配置用于模拟输入的基准电压（即输入范围的最大值）。选项有:
+//DEFAULT：默认5V（Ocrobot控制板为5V）或3.3伏特（Ocrobot控制板为3.3V）为基准电压。
+//INTERNAL：在ATmega168和ATmega328上以1.1V为基准电压，以及在ATmega8上以2.56V为基准电压（Ocrobot Mega无此选项）
+////INTERNAL1V1：以1.1V为基准电压（此选项仅针对Ocrobot Mega）
+//INTERNAL2V56：以2.56V为基准电压（此选项仅针对Ocrobot Mega）
+//EXTERNAL：以AREF引脚（0至5V）的电压作为基准电压。
+//参数
+//type：使用哪种参考类型（DEFAULT, INTERNAL, INTERNAL1V1, INTERNAL2V56,INTERNAL4V096 或者 EXTERNAL）。
+
+	uint32_t guid = (GUID3 << 24) | (GUID2 << 16) | (GUID1 << 8) | GUID0; // 给guid赋值唯一ID
+	Serial.println(guid); // 串口输出唯一ID
+
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+	// read the input on analog pin 0:
+	int sensorValue = analogRead(A0);
+	light = map(sensorValue, min, max, 0, 7);
+	// Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+//  float voltage = sensorValue * (3.3 / 4064.0);
+	// print out the value you read:
+	Serial.print(sensorValue);
+	Serial.print("  ");
+	Serial.print(constrain(sensorValue, min, max));
+	Serial.print("  ");
+	Serial.println(light);
+	delay(1000);
+}
